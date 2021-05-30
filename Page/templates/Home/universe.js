b8bc9b5363fe592e4b1cb60819853ddf7b99ccc3
João Marcos <joaoramalho30@yahoo.com.br>
2021-05-30 20:24:46 -0300

    'first commit'

---
+++ Page/templates/Home/universe.js
@@ -0,0 +1,31 @@
+
+function changeColor(id)
+{
+  document.getElementById(id).style.color = "#7cb0bc"
+  
+}
+
+function changeColor2(id)
+{
+  document.getElementById(id).style.color = "#ffe88a"
+}
+
+function changeColor3(id)
+{
+  document.getElementById(id).style.color = "#d75a4a"
+}
+
+function replace() {
+  document.body.style.backgroundImage = 'url("../../assets/img/bg2.png")'
+  
+}
+
+function replace2() {
+  document.body.style.backgroundImage = 'url("../../assets/img/bg1.png")'
+  
+}
+
+function replace3() {
+  document.body.style.backgroundImage = 'url("../../assets/img/bg3.png")'
+  
+}
